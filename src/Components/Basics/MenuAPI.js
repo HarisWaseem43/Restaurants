f0282dc5b89c202ca83109b0790b6b2f33b0105c
Haris <hariswaseem143@gmail.com>
2023-05-23 12:23:20 +0500

    first commit

---
+++ src/Components/Basics/MenuAPI.js
@@ -0,0 +1,95 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "100 Rs",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Allu Pakoda",
+      category: "evening",
+      price: "200 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "Corn",
+      category: "breakfast",
+      price: "50 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "Channey",
+      category: "lunch",
+      price: "120 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "Pizza",
+      category: "evening",
+      price: "1200 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Vegetabele Plater",
+      category: "dinner",
+      price: "999 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "450 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Biryani",
+      category: "lunch",
+      price: "320 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samosa",
+      category: "evening",
+      price: "80 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "../images/samosa.jpg",
+      name: "Toast",
+      category: "toast",
+      price: "250 Rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+
+export default Menu;
